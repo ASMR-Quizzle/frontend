1ff6bc335c5721f2e32a76abcab5fe09ab984860
SiddhiWakchaure <60885429+SiddhiWakchaure@users.noreply.github.com>
2022-08-17 03:31:43 +0530

    adding login page

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,62 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="w-full">
-      <p className="text-3xl text-center font-primary text-statusGreen">
-        Hello World!
-      </p>
+    <div className="flex flex-col font-primary">
+      <main className="flex flex-col">
+        {/* full screen block */}
+        <div className="flex w-full min-h-screen">
+          {/* Sign In Section */}
+          <div className="flex flex-col bg-white w-2/3 justify-center">
+            <div className="w-1/2 ml-52">
+              <div className="flex flex-col">
+                <p className="font-bold text-3xl pb-6 text-black">Sign In</p>
+                <p className="font-medium text-xl text-black mb-8">
+                  Login to your account to start adding questions
+                </p>
+                <div className="border-textSecondary border rounded-lg flex mb-6">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email or Phone Number"
+                    className="outline-none m-2 flex-1 text-textSecondary text-sm font-light"
+                  ></input>
+                </div>
+                <div className="border-textSecondary border rounded-lg flex">
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    className="outline-none m-2 flex-1 text-textSecondary text-sm font-light"
+                  ></input>
+                </div>
+                <div className="my-5 font-normal text-right text-sm text-textPrimary">
+                  <a href="#">Forgot Password?</a>
+                </div>
+                <div className="bg-primary flex justify-center py-2 rounded-lg">
+                  <p className="font-semibold text-xl text-white">Sign In</p>
+                </div>
+                <p className="text-black pt-3">
+                  Don't have an account?
+                  <a href="#">
+                    <span className="text-primary font-medium"> Register</span>
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* Vector Section */}
+          <div className="flex flex-col bg-primary w-1/3 items-center justify-center">
+            <img src="/signinPgVector.png" width="300px" className=""></img>
+            <p className="text-white font-medium text-2xl w-2/3 text-center">
+              Just a click here and another there and you are logged in!
+            </p>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
